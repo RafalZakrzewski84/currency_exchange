8ea8e25028ade2883c1c4faa77c92881b8f2220e
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-09 09:50:55 +0200

    creating table component

---
+++ src/Components/TableComp.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React, { Component } from 'react';
+
+class TableComp extends Component {
+	render() {
+		console.log('props', this.props);
+		return (
+			<div>
+				<h1>Hello form Table</h1>
+			</div>
+		);
+	}
+}
+
+export default TableComp;
